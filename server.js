b6f352f11f9d9a520fa8d7344213f5ff376673a9
Gopi1311 <gopinathk1311@gmail.com>
2025-10-08 18:33:25 +0530

    cors

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+
 const allowedOrigins = [
-    'https://admindashbord03.netlify.app/',
+    'https://admindashbord03.netlify.app',
 ];
+
 const corsOptions = {
     origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true); 
-        }
+        if (!origin) return callback(null, true); 
         if (allowedOrigins.indexOf(origin) === -1) {
             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
             return callback(new Error(msg), false);
@@ -27,6 +27,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
 
 const PORT = process.env.PORT || 5000;
 
